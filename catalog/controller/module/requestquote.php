3a9846c5061f58531f878615efccda46d4405c77
Arivu <arivazhagan.pandi@arkinfotec.com>
2017-06-10 16:37:22 +0530

    Request Quote

--- catalog/controller/module/requestquote.php
+++ catalog/controller/module/requestquote.php
@@ -19,20 +19,9 @@ class ControllerModuleRequestquote extends Controller
 
         $products = $this->cart->getProducts();
         $IP = $this->request->post;
+        $IP['products'] = $products;
 
-        $message = "<b><u>Request Quote:</u></b> \n\n";
-        $message .= "<b>Name : </b>" . $IP['name'] . "\n";
-        if ($IP['email'])
-            $message .= "<b>Email : </b>" . $IP['email'] . "\n";
-        if ($IP['phone'])
-            $message .= "<b>Phone : </b>" . $IP['phone'] . "\n";
-        if ($IP['message'])
-            $message .= "<b>Message : </b>" . $IP['name'] . "\n";
-
-        $message .= "<b>Products : </b>" . $IP['name'] . "\n";
-        foreach ($products as $product) {
-            $message .= "<b>Name : </b>" . $product['name'] . "<b>Quantity : </b>" . $product['quantity'] . "\n";
-        }
+        $message = $this->prepareMailTemplate($IP);
 
         $mail = new Mail();
         $mail->protocol = $this->config->get('config_mail_protocol');
@@ -44,8 +33,90 @@ class ControllerModuleRequestquote extends Controller
         $mail->smtp_timeout = $this->config->get('config_mail_smtp_timeout');
 
         $mail->setTo($this->config->get('config_email'));
+        $mail->setFrom('noreply@virattoys.com');
+        $mail->setSender($this->config->get('config_name'));
         $mail->setSubject("New Request Quote Notification");
-        $mail->setText($message);
+        $mail->setHtml($message);
         $mail->send();
+
+        $this->response->redirect($this->url->link('module/requestquote/thankyou'));
+    }
+
+    public function thankyou()
+    {
+        $this->cart->clear();
+
+        unset($this->session->data['shipping_method']);
+        unset($this->session->data['shipping_methods']);
+        unset($this->session->data['payment_method']);
+        unset($this->session->data['payment_methods']);
+        unset($this->session->data['guest']);
+        unset($this->session->data['comment']);
+        unset($this->session->data['order_id']);
+        unset($this->session->data['coupon']);
+        unset($this->session->data['reward']);
+        unset($this->session->data['voucher']);
+        unset($this->session->data['vouchers']);
+        unset($this->session->data['totals']);
+
+        $this->document->setTitle('Your quote has been sent successfully');
+
+        $data['breadcrumbs'] = array();
+
+        $data['breadcrumbs'][] = array(
+            'text' => $this->language->get('text_home'),
+            'href' => $this->url->link('common/home')
+        );
+
+        $data['heading_title'] = 'Your quote has been sent successfully';
+        $data['text_message'] = sprintf('<p>Your quote has been successfully processed!</p><p>Please direct any questions you have to the <a href="%s">store owner</a>.</p><p>Thanks for contacting with us online!</p>', $this->url->link('information/contact'));
+
+        $data['button_continue'] = 'Goto Site';
+
+        $data['continue'] = $this->url->link('common/home');
+
+        $data['column_left'] = $this->load->controller('common/column_left');
+        $data['column_right'] = $this->load->controller('common/column_right');
+        $data['content_bottom'] = $this->load->controller('common/content_bottom');
+        $data['footer'] = $this->load->controller('common/footer');
+        $data['header'] = $this->load->controller('common/header');
+
+        if (file_exists(DIR_TEMPLATE . $this->config->get('config_template') . '/template/module/requestquote/thankyou.tpl')) {
+            $this->response->setOutput($this->load->view($this->config->get('config_template') . '/template/module/requestquote/thankyou.tpl', $data));
+        } else {
+            $this->response->setOutput($this->load->view('default/template/module/requestquote/thankyou.tpl', $data));
+        }
+    }
+
+    protected function prepareMailTemplate($Params)
+    {
+        $message = '<table style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; background-color: #f6f6f6; margin: 0;" bgcolor="#f6f6f6"><tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0;" valign="top"></td>
+		<td class="container" width="600" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; display: block !important; max-width: 600px !important; clear: both !important; margin: 0 auto;" valign="top">
+			<div class="content" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; max-width: 600px; display: block; margin: 0 auto; padding: 20px;">
+				<table class="main" width="100%" cellpadding="0" cellspacing="0" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; border-radius: 3px; background-color: #fff; margin: 0; border: 1px solid #e9e9e9;" bgcolor="#fff"><tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-wrap aligncenter" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: center; margin: 0; padding: 20px;" align="center" valign="top">
+							<table width="100%" cellpadding="0" cellspacing="0" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-block" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+										<img src="http://virattoys.com/image/catalog/logo.png" alt="ViratToys" title="ViratToys" style="text-align: center; margin: 40px 0 0;" align="center" />
+									</td>
+								</tr><tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-block" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 0 0 20px;" valign="top">
+										<h2 class="aligncenter" style="font-family: \'Helvetica Neue\',Helvetica,Arial,\'Lucida Grande\',sans-serif; box-sizing: border-box; font-size: 24px; color: #000; line-height: 1.2em; font-weight: 400; text-align: center; margin: 40px 0 0;" align="center">New Quote Request</h2>
+									</td>
+								</tr><tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td class="content-block aligncenter" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: center; margin: 0; padding: 0 0 20px;" align="center" valign="top">
+										<table class="invoice" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; text-align: left; width: 80%; margin: 40px auto;"><tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 5px 0;" valign="top">' . $Params['name'] . '<br style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;" />' . $Params['email'] . '<br style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;" />' . $Params['phone'] . '<br style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;" />' . $Params['message'] . '</td>
+											</tr><tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;"><td style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; margin: 0; padding: 5px 0;" valign="top">
+													<table class="invoice-items" cellpadding="0" cellspacing="0" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; width: 100%; margin: 0;"><tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+													    <td style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; font-weight: bold; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" valign="top">Product Name</td>
+                                                        <td class="alignright" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; font-weight: bold; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" align="right" valign="top">Quantity</td>
+                                                    </tr>';
+        foreach ($Params['products'] as $product) {
+            $message .= '<tr style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; margin: 0;">
+													    <td style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" valign="top">' . $product['name'] . '</td>
+                                                        <td class="alignright" style="font-family: \'Helvetica Neue\',Helvetica,Arial,sans-serif; box-sizing: border-box; font-size: 14px; vertical-align: top; text-align: right; border-top-width: 1px; border-top-color: #eee; border-top-style: solid; margin: 0; padding: 5px 0;" align="right" valign="top">' . $product['quantity'] . '</td>
+                                                    </tr>';
+        }
+
+
+        $message .= '</table></td></tr></table></td></tr></table></td></tr></table></div></td><td valign="top"></td></tr></table>';
+
+        return $message;
     }
 }
